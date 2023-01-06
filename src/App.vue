4bd062ff030e1dc74402e0d6ad5c5c897877aabf
MikhailEzhov <Hammer.perm@gmail.com>
2023-01-06 16:41:26 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>hi</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+* {
+  padding: 0px;
+  margin: 0px;
+  border: none;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+</style>
