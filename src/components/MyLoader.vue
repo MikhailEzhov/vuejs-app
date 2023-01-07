9723d52fe631d8384246b7b15efea85371edd06d
MikhailEzhov <Hammer.perm@gmail.com>
2023-01-07 12:08:07 +0500

    feat: added component showing loading

---
+++ src/components/MyLoader.vue
@@ -0,0 +1,31 @@
+<template>
+  <span class="loader"></span>
+</template>
+
+<script>
+export default {
+  name: "MyLoader",
+};
+</script>
+
+<style>
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 5px solid;
+  border-color: #000000 transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
