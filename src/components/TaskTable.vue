979376afd42c1738e5d0bf24dbfaec5e0a7bccb5
MikhailEzhov <Hammer.perm@gmail.com>
2023-01-07 16:19:25 +0500

    feat: added task table component

---
+++ src/components/TaskTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">Title</th>
+        <th class="text-left">Completed</th>
+        <th class="text-left">change</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="task in tasks" :key="task.id">
+        <td>{{ task.title }}</td>
+        <td v-if="task.completed" class="text-center">
+          <v-icon icon="mdi-check" color="green" />
+        </td>
+        <td v-else class="text-center">
+          <v-icon icon="mdi-minus" color="red" />
+        </td>
+        <td class="text-center">
+          <v-btn size="x-small" icon="mdi-pencil" color="info"></v-btn>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+export default {
+  name: "TaskTable",
+
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
